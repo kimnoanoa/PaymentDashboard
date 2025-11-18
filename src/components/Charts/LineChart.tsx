5b453a9d83c2e876a41d252b0d7e5d93d591b76c
NOAH <noamom00@naver.com>
2025-11-18 14:38:51 +0900

    그래프 잘려보이는 듯한 느낌 수정

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -8,6 +8,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+
 interface Props {
   data: { date: string; amount: number }[];
 }
@@ -32,13 +33,20 @@ function DailyLineChart({ data }: Props) {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md mt-10">
       <h2 className="text-xl font-bold mb-4">일자별 결제 금액</h2>
+
       <ResponsiveContainer width="100%" height={300}>
         <LineChart data={data}>
           <CartesianGrid strokeDasharray="3 3" stroke="#C9D9C9" />
-          <XAxis dataKey="date" stroke="#24553A" />
-          <YAxis stroke="#24553A" />
 
-          {/* 커스텀 툴팁 사용 */}
+          <XAxis dataKey="date" stroke="#24553A" />
+
+          {/* y축 */}
+          <YAxis
+            stroke="#24553A"
+            tick={{ fontSize: 10 }}                          
+            tickFormatter={(v) => v.toLocaleString()} 
+          />
+
           <Tooltip content={<CustomTooltip />} />
 
           <Line
