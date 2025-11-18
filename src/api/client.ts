6c5c6ddd92a7a75fc26558b34e6a482d1b84e7d3
NOAH <noamom00@naver.com>
2025-11-18 19:14:12 +0900

    proxy

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://recruit.paysbypays.com/api/v1",
+  baseURL: "/api",
   timeout: 5000,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: {
-    //   "/api": {
-    //     target: "https://recruit.paysbypays.com",
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "https://recruit.paysbypays.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+      },
+    },
   },
 });
